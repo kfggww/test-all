085046b5c183dedbe776042cbc0a9e545283a6d8
kfggww <kfggww@outlook.com>
2022-10-09 17:54:30 +0800

    Implements free memory methods of buddy system.

--- 03_buddy_system/buddy.c
+++ 03_buddy_system/buddy.c
@@ -4,55 +4,80 @@
 #include <assert.h>
 
 #define PGSIZE 4096
+#define RANGE_BETWEEN(c, a, b) ((unsigned long)(c) >= (unsigned long)(a) && (unsigned long)(c) < (unsigned long)(b))
+#define OFFSET_FROM(base, offset) ((unsigned long)(base) + (unsigned long)(offset))
+#define HALF_OF(s) (((unsigned long)(s)) / 2)
+#define ALLOC_BLOCK()                                                             \
+    ({                                                                            \
+        struct buddy_block *__block = (struct buddy_block *)buddy_block_buf_mark; \
+        buddy_block_buf_mark = (void *)(__block + 1);                             \
+        __block;                                                                  \
+    })
 
-enum vm_block_state
+#define buddy_list_for_earch(pos, head) \
+    for (struct buddy_block *pos = (head)->next; pos != (head); pos = pos->next)
+
+enum buddy_block_state
 {
     BLK_FREE,
     BLK_USED,
     BLK_SPLIT,
 };
 
-struct buddy_vm_block
+struct buddy_block
 {
     void *start;
     size_t size;
-    enum vm_block_state state;
-    struct buddy_vm_block *next;
-    struct buddy_vm_block *prev;
-    struct buddy_vm_block *left;
-    struct buddy_vm_block *right;
+    enum buddy_block_state state;
+    struct buddy_block *next;
+    struct buddy_block *prev;
+    struct buddy_block *parent;
+    struct buddy_block *left;
+    struct buddy_block *right;
 };
 
-static struct buddy_vm_block vmblk_head;
-static void *buddy_buf;
-static void *buddy_buf_mark;
+static struct buddy_block buddy_block_head;
+static void *buddy_block_buf;
+static void *buddy_block_buf_mark;
 
-void buddy_split_block(struct buddy_vm_block *root)
+static void buddy_block_add_prev(struct buddy_block *head, struct buddy_block *node)
 {
-    struct buddy_vm_block *left = (struct buddy_vm_block *)buddy_buf_mark;
-    buddy_buf_mark = (void *)((unsigned long)buddy_buf_mark + sizeof(struct buddy_vm_block));
-    struct buddy_vm_block *right = (struct buddy_vm_block *)buddy_buf_mark;
-    buddy_buf_mark = (void *)((unsigned long)buddy_buf_mark + sizeof(struct buddy_vm_block));
+    node->next = head;
+    node->prev = head->prev;
+    head->prev->next = node;
+    head->prev = node;
+}
+
+static void buddy_split_block(struct buddy_block *root)
+{
+    struct buddy_block *left = ALLOC_BLOCK();
+    struct buddy_block *right = ALLOC_BLOCK();
+    // make sure buddy blocks buf uses no more than one page
+    assert((unsigned long)buddy_block_buf_mark - (unsigned long)buddy_block_buf < PGSIZE);
 
     root->state = BLK_SPLIT;
     root->left = left;
     root->right = right;
 
     left->next = left->prev = left;
+    left->parent = root;
     left->left = left->right = NULL;
     left->state = BLK_FREE;
     left->start = root->start;
     left->size = root->size / 2;
 
     right->next = right->prev = right;
+    right->parent = root;
     right->left = right->right = NULL;
     right->state = BLK_FREE;
     right->start = root->start + root->size / 2;
     right->size = root->size / 2;
 }
 
-void *buddy_alloc1(struct buddy_vm_block *root, size_t size)
+static void *buddy_block_alloc(struct buddy_block *root, size_t size)
 {
+    if (root == NULL)
+        return NULL;
 
     void *buf = NULL;
     switch (root->state)
@@ -60,10 +85,10 @@ void *buddy_alloc1(struct buddy_vm_block *root, size_t size)
     case BLK_USED:
         return NULL;
     case BLK_SPLIT:
-        buf = buddy_alloc1(root->left, size);
+        buf = buddy_block_alloc(root->left, size);
         if (buf != NULL)
             return buf;
-        return buddy_alloc1(root->right, size);
+        return buddy_block_alloc(root->right, size);
     case BLK_FREE:
         if (2 * size > root->size)
         {
@@ -73,39 +98,14 @@ void *buddy_alloc1(struct buddy_vm_block *root, size_t size)
         else
         {
             buddy_split_block(root);
-            buf = buddy_alloc1(root->left, size);
+            buf = buddy_block_alloc(root->left, size);
             if (buf != NULL)
                 return buf;
-            return buddy_alloc1(root->right, size);
+            return buddy_block_alloc(root->right, size);
         }
     default:
         return buf;
     }
-}
-
-void list_add_prev(struct buddy_vm_block *head, struct buddy_vm_block *node)
-{
-    node->next = head;
-    node->prev = head->prev;
-    head->prev->next = node;
-    head->prev = node;
-}
-
-void buddy_init()
-{
-    vmblk_head.next = vmblk_head.prev = &vmblk_head;
-    vmblk_head.state = BLK_USED;
-    vmblk_head.start = NULL;
-    vmblk_head.size = 0;
-    vmblk_head.left = vmblk_head.right = NULL;
-
-    buddy_buf = malloc(PGSIZE);
-    buddy_buf_mark = buddy_buf;
-    assert(buddy_buf != NULL);
-}
-
-void buddy_fini()
-{
 }
 
 void *buddy_alloc(size_t size)
@@ -115,33 +115,100 @@ void *buddy_alloc(size_t size)
 
     void *alloc_buf = NULL;
 
-    for (struct buddy_vm_block *pos = vmblk_head.next; pos != &vmblk_head; pos = pos->next)
+    buddy_list_for_earch(pos, &buddy_block_head)
     {
-        alloc_buf = buddy_alloc1(pos, size);
+        alloc_buf = buddy_block_alloc(pos, size);
         if (alloc_buf != NULL)
             return alloc_buf;
     }
 
-    struct buddy_vm_block *new_root = (struct buddy_vm_block *)buddy_buf_mark;
-    buddy_buf_mark = (void *)((unsigned long)buddy_buf_mark + sizeof(struct buddy_vm_block));
-    list_add_prev(&vmblk_head, new_root);
+    struct buddy_block *root = ALLOC_BLOCK();
+    buddy_block_add_prev(&buddy_block_head, root);
 
-    new_root->start = malloc(PGSIZE);
-    assert(new_root->start != NULL);
-    new_root->state = BLK_FREE;
-    new_root->size = PGSIZE;
-    new_root->left = new_root->right = NULL;
+    root->start = malloc(PGSIZE);
+    assert(root->start != NULL);
+    root->state = BLK_FREE;
+    root->size = PGSIZE;
+    root->parent = NULL;
+    root->left = root->right = NULL;
 
-    return buddy_alloc1(new_root, size);
+    return buddy_block_alloc(root, size);
 }
 
-int buddy_free(void *p)
+static void buddy_blkcpy_top2free(struct buddy_block *blk_free)
 {
+    struct buddy_block *blk_top = (struct buddy_block *)buddy_block_buf_mark - 1;
+
+    *blk_free = *blk_top;
+    if (blk_top == blk_top->parent->left)
+    {
+        blk_top->parent->left = blk_free;
+    }
+    else
+    {
+        blk_top->parent->right = blk_free;
+    }
+
+    buddy_block_buf_mark = (void *)blk_top;
+}
+
+static int buddy_block_free(struct buddy_block *root, void *p)
+{
+    if (root == NULL)
+        return -1;
+
+    int res = -1;
+    switch (root->state)
+    {
+    case BLK_FREE:
+        return -1;
+    case BLK_USED:
+        if (root->start != p)
+            return -1;
+        else
+        {
+            root->state = BLK_FREE;
+            return 0;
+        }
+    case BLK_SPLIT:
+        if (p < (void *)((unsigned long)root->start + HALF_OF(root->size)))
+            res = buddy_block_free(root->left, p);
+        else
+            res = buddy_block_free(root->right, p);
+        break;
+    default:
+        return -1;
+    }
+
+    if (res)
+        return res;
+
+    if (root->left != NULL && root->left->state == BLK_FREE && root->right != NULL && root->right->state == BLK_FREE)
+    {
+        buddy_blkcpy_top2free(root->left > root->right ? root->left : root->right);
+        buddy_blkcpy_top2free(root->left < root->right ? root->left : root->right);
+        root->left = root->right = NULL;
+        root->state = BLK_FREE;
+    }
 
     return 0;
 }
 
-void buddy_block_info(struct buddy_vm_block *root)
+int buddy_free(void *p)
+{
+    int res = -1;
+    buddy_list_for_earch(pos, &buddy_block_head)
+    {
+        if (RANGE_BETWEEN(p, pos->start, OFFSET_FROM(pos->start, PGSIZE)))
+        {
+            res = buddy_block_free(pos, p);
+            break;
+        }
+    }
+    return res;
+}
+
+static void buddy_block_info(struct buddy_block *root)
 {
     if (root == NULL)
         return;
@@ -155,8 +222,36 @@ void buddy_block_info(struct buddy_vm_block *root)
 
 void buddy_info()
 {
-    for (struct buddy_vm_block *pos = vmblk_head.next; pos != &vmblk_head; pos = pos->next)
+    buddy_list_for_earch(pos, &buddy_block_head)
     {
         buddy_block_info(pos);
     }
+}
+
+void buddy_init()
+{
+    // buddy_block_head act as a guard
+    buddy_block_head.next = buddy_block_head.prev = &buddy_block_head;
+    buddy_block_head.state = BLK_USED;
+    buddy_block_head.start = NULL;
+    buddy_block_head.size = 0;
+    buddy_block_head.parent = NULL;
+    buddy_block_head.left = buddy_block_head.right = NULL;
+
+    // allocate one page size of memory to store buddy blocks
+    // we assume that one page is enough for simplicity
+    buddy_block_buf = malloc(PGSIZE);
+    buddy_block_buf_mark = buddy_block_buf;
+    assert(buddy_block_buf != NULL);
+}
+
+void buddy_fini()
+{
+    buddy_list_for_earch(root, &buddy_block_head)
+    {
+        assert(root != NULL && root->start != NULL && root->state == BLK_FREE);
+        free(root->start);
+    }
+
+    free(buddy_block_buf);
 }
--- 03_buddy_system/main.c
+++ 03_buddy_system/main.c
@@ -8,13 +8,31 @@ int main(int argc, char **argv)
     buddy_init();
 
     size_t sizes[] = {2000, 1000, 500, 200};
-    for (int i = 0; i < sizeof(sizes) / sizeof(size_t); ++i)
+    const int N = sizeof(sizes) / sizeof(size_t);
+    void *bufs[N];
+
+    for (int n = 0; n < 2; ++n)
     {
-        void *pbuf = buddy_alloc(sizes[i]);
-        printf("#%d: %p\n", i, pbuf);
+        printf("\n\n=================== test #%d: ===================\n\n", n);
+        printf("buddy alloc result:\n\n");
+        for (int i = 0; i < N; ++i)
+        {
+            bufs[i] = buddy_alloc(sizes[i]);
+            printf("#%d: %p\n", i, bufs[i]);
+        }
+
+        printf("\n\nblock info before free:\n\n");
+        buddy_info();
+
+        for (int i = 0; i < N; ++i)
+        {
+            buddy_free(bufs[i]);
+        }
+
+        printf("\n\nblock info after free:\n\n");
+        buddy_info();
     }
 
-    buddy_info();
     buddy_fini();
     return 0;
 }
