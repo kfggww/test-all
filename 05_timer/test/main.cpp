0375fbac5a14072631e0819cda7e581cd14a6f8c
kfggww <kfggww@outlook.com>
2022-11-24 10:14:38 +0800

    Test timers in multi-threads.

--- 05_timer/test/main.cpp
+++ 05_timer/test/main.cpp
@@ -31,6 +31,8 @@
 // }
 
 std::atomic<int> counter = 0;
+pthread_mutex_t cond_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_cb3_done = PTHREAD_COND_INITIALIZER;
 
 void callback1(void *data) {
     counter += 1;
@@ -48,6 +50,7 @@ void callback3(void *data) {
     counter += 1;
     pthread_t *thread = (pthread_t *)data;
     printf("Thread[%lu] callback3\n", *thread);
+    pthread_cond_signal(&cond_cb3_done);
 }
 
 void *thread_entry(void *data) {
@@ -55,7 +58,9 @@ void *thread_entry(void *data) {
     timer.AddCallback({callback1, data, 2500});
     timer.AddCallback({callback2, data, 2000});
     timer.AddCallback({callback3, data, 3000});
-    sleep(10);
+    pthread_mutex_lock(&cond_lock);
+    pthread_cond_wait(&cond_cb3_done, &cond_lock);
+    pthread_mutex_unlock(&cond_lock);
     printf("Thread[%lu] stoped\n", *(unsigned long *)(data));
 
     return NULL;
@@ -63,7 +68,7 @@ void *thread_entry(void *data) {
 
 int main(int argc, char **argv) {
 
-    TimerHighResolution timer;
+    // TimerHighResolution timer;
 
     pthread_t t1, t2;
     pthread_create(&t1, NULL, thread_entry, &t1);
